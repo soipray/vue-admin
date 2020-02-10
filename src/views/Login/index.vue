76e2900d84bce38b1520e7963accb45ede68c2c1
soipray <462793431@qq.com>
2020-02-10 15:56:24 +0800

    构建项目 配置全局样式 路由 element-ui

---
+++ src/views/Login/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="login">
+        login<el-button>默认按钮</el-button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Login'
+}
+</script>
+
+<style lang="scss" scoped>
+#login {
+    height: 100vh;
+    background-color: #344a5f;
+}
+</style>
